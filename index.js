c89b6d8fd2d9cffee5efedca6db3d16ac9d97a7b
Cameron <Cameron@dansclan.com>
2022-10-13 14:37:06 -0700

    commit tree 790995445ef0df7a750a04f2fd9946c47e51eeaa

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Code your solutions in this file
+const gifts = [`teddy bear`, `drone`, `doll`];
+
+function wrapGifts(gifts){
+    for (let i = 0; i < gifts.length; i++){
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+
+wrapGifts(gifts);
+
+const cards = [];
+
+function writeCards(name){
+    for (let i = 0; i < name.length; i++){
+        cards.splice(i, 0, `Thank you, ${name[i]}, for the wonderful surprise gift!`)
+    }
+    return cards;
+}
+
+function countDown(i){
+    for(i; i >= 0; i--){
+        console.log(i);
+    }
+}
